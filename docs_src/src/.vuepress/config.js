f79556f9ebfa64c7291ff7ae3ca773b6d0ec7e31
Andrew Lalis <andrewlalisofficial@gmail.com>
2021-11-02 19:30:44 +0100

    Added base url

--- docs_src/src/.vuepress/config.js
+++ docs_src/src/.vuepress/config.js
@@ -20,6 +20,8 @@ module.exports = {
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
+  
+  base: '/ConcordServer/',
 
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
